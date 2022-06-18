2f86aff710b3d18388b3fda8bd8b03d92a843a6c
Cédric MILLET <32933971+cedricmillet@users.noreply.github.com>
2022-06-18 12:18:37 +0200

    Create code.js

---
+++ 136. Single Number/code.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    /*
+    //  BAD WAY - TOO LONG
+    const count = [];
+    for(let i=0;i<nums.length;i++) {
+        const f = count.find(c => c.n === nums[i]);
+        if(f) f.c++;
+        else count.push({n: nums[i], c: 1});
+    }
+    console.log(count, count.find(c => c.c !== 2))
+    return count.find(c => c.c !== 2).n*/
+    
+    // OPTIMIZED WAY  
+    return nums.reduce((a,b) => a^b)
+};
