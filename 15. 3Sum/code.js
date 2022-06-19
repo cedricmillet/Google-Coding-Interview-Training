1ec37389b122f5ce408e83ba1275bde8269ccc42
Cédric MILLET <32933971+cedricmillet@users.noreply.github.com>
2022-06-19 11:46:06 +0200

    Update code.js

--- 15. 3Sum/code.js
+++ 15. 3Sum/code.js
@@ -3,6 +3,9 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+    
+    /*
+    //  BAD WAY !!!!!!
     const len = nums.length;
     const triplets = [];
     const target = 0;
@@ -34,4 +37,41 @@ var threeSum = function(nums) {
     }
     
     return triplets.map(t => t.values);
+    */
+    
+    
+    
+    const len = nums.length;
+    nums = nums.sort((a,b) => a-b);
+    console.log(nums)
+    const result = [];
+    
+    
+    for(let i=0;i<len-2;i++) {
+        //  duplicate i values
+		if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+        // right window [j,k]
+        for(let j=i+1, k=len-1;j<k;) {
+            const sum = nums[i] + nums[j] + nums[k];
+            if(sum===0) {
+                result.push([nums[i], nums[j], nums[k]]);
+                j++;
+                k--;
+                // Prevent duplicate
+                while (j < k && nums[j] == nums[j - 1])
+					j++;
+				while (j < k && nums[k] == nums[k + 1])
+					k--;
+            } else if(sum>0) {
+                k--;    
+            } else {
+                j++;    
+            }
+            
+        }
+    }
+    //console.log(result);
+    return result;
 };
