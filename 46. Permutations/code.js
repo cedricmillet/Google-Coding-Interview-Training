f369d93aca57d84027d6d93cc4e5a726f180f450
Cédric MILLET <32933971+cedricmillet@users.noreply.github.com>
2022-06-26 14:45:26 +0200

    Create code.js

---
+++ 46. Permutations/code.js
@@ -0,0 +1,32 @@
+Array.prototype.swap = function (x,y) {
+  var b = this[x];
+  this[x] = this[y];
+  this[y] = b;
+  return this;
+}
+
+/** Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ * @param {number[]} nums
+ * @return {number[][]}
+ Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ */
+var permute = function(nums) {    
+    
+    const dfs = (nums, l, r, results=[]) => {
+        if(l == r) {
+            results.push([...nums]);
+            return;
+        };
+        for(let i=l; i<r; i++) {
+            nums.swap(l,i);
+            dfs(nums, l+1, r, results);
+            nums.swap(l,i);
+        }
+    }
+    
+    result = [];
+    dfs(nums, 0, nums.length, result);
+    
+    return result;
+};
